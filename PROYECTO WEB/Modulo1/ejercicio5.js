841b1cba3352322a6de5e83f05bb2c0e7a0b28fc
Rafagrn <151873823+Rafagrn@users.noreply.github.com>
2024-01-30 15:12:27 -0500

    actividad

---
+++ PROYECTO WEB/Modulo1/ejercicio5.js
@@ -0,0 +1,36 @@
+/**Practivad 1 ejercicio 5  calcule el valor Segun sueldo */
+
+let sueldo1 = 699999;
+let sueldo2 =700000;
+let sueldo3 = 999999;
+let sueldo4 =1000000;
+const pension1 = 0.02;
+const pension2 = 0.04;
+const pension3 = 0.06;
+const auxt1 = 0.06;
+const valpen1 =(sueldo1*pension1);
+console.log("Sueldo base $",sueldo1)
+console.log("Descuento Pension$" ,valpen1)
+
+const valtrans1 =(sueldo1*auxt1);
+console.log("Descuento transporte",valtrans1)
+
+  
+/**Practivad 1 ejercicio 5  calcule el valor Segun sueldo */
+
+let sueldo1 = 699999;
+let sueldo2 =700000;
+let sueldo3 = 999999;
+let sueldo4 =1000000;
+const pension1 = 0.02;
+const pension2 = 0.04;
+const pension3 = 0.06;
+const auxt1 = 0.06;
+const valpen1 =(sueldo1*pension1);
+console.log("Sueldo base $",sueldo1)
+console.log("Descuento Pension$" ,valpen1)
+
+const valtrans1 =(sueldo1*auxt1);
+console.log("Descuento transporte",valtrans1)
+
+  
